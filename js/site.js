115c0efb594ac8fca9beadd132329eda33754f8c
mosaicosbahia <thalitaferrazribeiro@gmail.com>
2017-01-17 09:41:56 -0300

    publish

---
+++ js/site.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function(){ 
+    jQuery("#gallery-projetos").unitegallery({
+        gallery_theme:"tiles"
+        ,tiles_type:"columns"
+    }); 
+    jQuery("#gallery-produtos").unitegallery({
+        gallery_theme:"tilesgrid"
+    }); 
+}); 
+        
